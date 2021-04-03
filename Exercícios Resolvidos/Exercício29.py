8e7dad259e4225a13b5774678e3f9580c070d471
Evandro Gubert <80432532+gubertevandro001@users.noreply.github.com>
2021-04-03 20:43:16 -0300

    Create Exercício29.py

---
+++ Exercícios Resolvidos/Exercício29.py
@@ -0,0 +1,19 @@
+# 29 - O Sr. Manoel Joaquim possui uma grande loja de artigos de R$ 1,99, com cerca de 10 caixas. Para agilizar o
+# cálculo de quanto cada cliente deve pagar ele desenvolveu um tabela que contém o número de itens que o cliente
+# comprou e ao lado o valor da conta. Desta forma a atendente do caixa precisa apenas contar quantos itens o cliente
+# está levando e olhar na tabela de preços. Você foi contratado para desenvolver o programa que monta esta tabela de
+# preços, que conterá os preços de 1 até 50 produtos, conforme o exemplo abaixo:
+
+#Lojas Quase Dois - Tabela de preços
+#1 - R$ 1.99
+#2 - R$ 3.98
+#...
+#50 - R$ 99.50
+
+quantidade_produtos = int(input('Informe a Quantidade de Produtos: '))
+valor_produto = float(input('Informe o Valor do Produto R$: '))
+
+print('==========Lojas Quase Dois - Tabela de Preços==========')
+
+for count in range(1,quantidade_produtos + 1):
+    print(f'{count} - R$ {count * valor_produto:.2f}')
